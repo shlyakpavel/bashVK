c5009b4a240b3a0e144e1cd4c51508d55e47e602
Pavel Shlyak <shlyak-pavel2011@ya.ru>
2017-07-01 18:12:47 +0400

    Made code more flexible. Curl is not hardcored anymore

--- api.sh
+++ api.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
+#You can always change it to something else(like wget). This function is only not to hardcore curl as the only network engine.
+function request {
+curl -s $1
+}
+
+
 function getvalue {
 grep $2 $1 | sed -e 's/<[^>]*>//g'
 }
 
 function status.set {
-curl -s "$api/status.set.xml?text=$2&v=$API_V&access_token=$1" > /dev/null
+request "$api/status.set.xml?text=$2&v=$API_V&access_token=$1" > /dev/null
 }
 
 function status.get.user {
-curl -s "$api/status.get.xml?user_id=$2&v=$API_V&access_token=$1" > $tmp_dir/status_$2.txt
+request "$api/status.get.xml?user_id=$2&v=$API_V&access_token=$1" > $tmp_dir/status_$2.txt
 getvalue $tmp_dir/status_$2.txt text
 }
 
 function status.get.group {
-curl -s "$api/status.get.xml?group_id=$2&v=$API_V&access_token=$1" > $tmp_dir/status_group_$2.txt
+request "$api/status.get.xml?group_id=$2&v=$API_V&access_token=$1" > $tmp_dir/status_group_$2.txt
 getvalue $tmp_dir/status_group_$2.txt text
 }
 
