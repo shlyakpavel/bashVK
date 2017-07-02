6a59700cd893b6577c910fa6f079be965b811286
Pavel Shlyak <shlyak-pavel2011@ya.ru>
2017-07-02 23:26:09 +0400

    Made auth much better

--- auth.sh
+++ auth.sh
@@ -1,9 +1,20 @@
 function auth {
 	APP_ID="12345"
 	URL="https://oauth.vk.com/authorize?client_id=$APP_ID&display=mobile&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,photos,status,messages,wall,groups&response_type=token&v=$API_V"
-#TODO: check if w3m is avaible. Make a diff loop to kill the browser in time
-	w3m $URL -reqlog 
-	token=$(grep Location: ~/.w3m/request.log | grep "#access_token" | tail -1 | cut -c11-)
+#TODO: check if w3m is avaible.
+	rm ./request.log
+	cp ~/.w3m/request.log ./request.log
+	w3m $URL -reqlog &
+	status=1
+	until [ $status -ne 1 ]; do
+		diff ~/.w3m/request.log ./request.log > diff.txt
+		grep "#access_token" ./diff.txt
+		status=$?
+		token=$(grep Location: ./diff.txt | grep "#access_token" | tail -1 | cut -c13-)
+        done
+#TODO killall is not the best option here
+	killall w3m
+	rm diff.txt
 	#xdg-open $URL && echo "Copy and paste url here" && read token
 	expires_in=$(url.getvar "$token" expires_in)
 	expr $expires_in + $(date +%s)  > token.key
