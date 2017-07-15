47e623161604df4bdf711e050bbe6a988da029fa
Pavel Shlyak <shlyak-pavel2011@ya.ru>
2017-07-15 13:32:18 +0400

    Longpoll processing

--- modules/vk_longpoll.sh
+++ modules/vk_longpoll.sh
@@ -1,10 +1,39 @@
 echo "Starting longpoll server"
 JSON=./JSON.sh
 
-get_json_data(){
+get_new_ts(){
 	data=$(grep "\[\"$2\"]" $tmp_dir/longpoll_$1.txt)
 	data=$(echo ${data##*]} | xargs)
 	echo $data
+}
+
+process_updates(){
+	args=""
+	cat $tmp_dir/longpoll_$1.txt
+	#TODO:	Make all updates count (now only the first one gets executed)
+	args_quantity=$(grep "\"updates\",0" $tmp_dir/longpoll_$1.txt | wc -l)
+	for (( c=1; c<$args_quantity; c++ ))
+	do
+		data=$(grep "\"updates\",0,$c" $tmp_dir/longpoll_$1.txt)
+		data=$(echo ${data##*]} | xargs)
+		args="$args $data"
+	done
+	cmd=$(grep "\"updates\",0,0" $tmp_dir/longpoll_$1.txt)
+	cmd=$(echo ${cmd##*]} | xargs)
+	echo -n "Executing command: $cmd "
+	echo "with args $args"
+	#TODO:	Change labels to commands and replace all the debug output to the modules
+	case $cmd in
+		1  ) echo "Замена флагов сообщения";;
+		2  ) echo "Установка флагов сообщения";;
+		3  ) echo "Сброс флагов сообщения";;
+		4  ) echo "Добавление нового сообщения.";;
+		7  ) echo "Прочтение всех исходящих сообщений";;
+		8  ) echo "Друг стал онлайн.";;
+		9  ) echo "Друг id стал оффлайн";;
+		61 ) echo "Пользователь набирает текст в диалоге";;
+		*  ) echo "Произошло что-то непонятное";;
+	esac
 }
 
 longpoll(){
@@ -19,7 +48,8 @@ longpoll(){
 	do
 		echo $ts
 		request "https://$server?act=a_check&key=$key&ts=$ts&wait=25&mode=2&version=2" | $JSON -b > $tmp_dir/longpoll_$ts.txt
-		new_ts=$(get_json_data $ts ts)
+		new_ts=$(get_new_ts $ts ts)
+		process_updates $ts
 		rm $tmp_dir/longpoll_$ts.txt
 		ts=$new_ts
 	done
