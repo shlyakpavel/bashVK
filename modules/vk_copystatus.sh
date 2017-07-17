b113fc210df416edb4134e587f1096d13ebfa7f6
Pavel Shlyak <shlyak-pavel2011@ya.ru>
2017-07-17 23:18:26 +0400

    Makes the module ready for translation. Cleanup

--- modules/vk_copystatus.sh
+++ modules/vk_copystatus.sh
@@ -1,10 +1,5 @@
 function copy_status {
-#while true
-#do
-	id=$(dialog --inputbox "Input user ID" 0 0 390061449 --output-fd 1)
+	id=$(dialog --inputbox $"uID" 0 0 390061449 --output-fd 1)
 	status=$(status.get.user "$access_token" $id)
-	#echo $status
 	status.set "$access_token" $(urlencode "$status") #$status
-#	sleep 10
-#done
 }
