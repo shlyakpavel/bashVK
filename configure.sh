8d7528dabc4b749cf8400fc7f0dc19c0949281c4
Pavel Shlyak <shlyak-pavel2011@ya.ru>
2017-11-14 13:20:25 +0300

    Configuration script enhancements

--- configure.sh
+++ configure.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-#Download json library
-wget https://raw.githubusercontent.com/dominictarr/JSON.sh/master/JSON.sh
+#Check dependencies
+function notfound {
+	echo "$1 not found. Aborting"
+	exit 1
+}
+[ -x "$(command -v dialog)" ] || notfound Dialog
+[ -x "$(command -v w3m)" ] || notfound W3m
+#Download json library if it is not presented in the current directory
+[ -f ./JSON.sh ] || wget https://raw.githubusercontent.com/dominictarr/JSON.sh/master/JSON.sh
 chmod a+x JSON.sh 
 #Generate translation files
 for FILENAME in *.po;
